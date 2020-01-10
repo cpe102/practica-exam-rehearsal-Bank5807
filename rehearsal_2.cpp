0f7329733df5af1f567b739222e1187c8f7942c4
Bank5807 <tanat_wip@cmu.ac.th>
2020-01-10 15:50:50 +0700

    rehearsal v1

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
 
+int main()
+{
+    double sum = 0, x;
+    while(x != 0){
+        cout << "Enter x: ";
+        cin  >> x;
+        if(x > 0){
+            sum = sum + x;
+        }
+    }
+    cout << "sum = " << sum;
+    return 0;
+}
